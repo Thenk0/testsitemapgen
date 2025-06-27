2095ff242b161888c244ccd277d329503ce94404
Vladislav <vladikvd@gmail.com>
2025-06-27 16:01:46 +0600

    xml fixes

--- src/generators/XMLGenerator.php
+++ src/generators/XMLGenerator.php
@@ -4,7 +4,8 @@ namespace Thenk0\SitemapParser\generators;
 
 use SimpleXMLElement;
 
-class XMLGenerator extends BaseGenerator {
+class XMLGenerator extends BaseGenerator
+{
 
     private SimpleXMLElement $xml;
 
@@ -12,15 +13,21 @@ class XMLGenerator extends BaseGenerator {
     {
         $this->xml = new SimpleXMLElement('<urlset/>');
     }
-    
-    protected function writeFileHeader($file, array $keys) {
+
+    protected function writeFileHeader($file, array $keys)
+    {
         $this->xml = new SimpleXMLElement('<urlset/>');
         $this->xml->addAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
         $this->xml->addAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
-        $this->xml->addAttribute('xsi:schemaLocation', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+        $this->xml->addAttribute(
+            'xsi:schemaLocation',
+            'file://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd',
+            'http://www.sitemaps.org/schemas/sitemap/0.9'
+        );
     }
 
-    protected function writeFileBody($file, array $pages) {
+    protected function writeFileBody($file, array $pages)
+    {
         foreach ($pages as $page) {
             $pagexml = $this->xml->addChild('url');
             foreach ($page as $key => $value) {
@@ -29,7 +36,8 @@ class XMLGenerator extends BaseGenerator {
         }
     }
 
-    protected function writeFileFooter($file) {
+    protected function writeFileFooter($file)
+    {
         fwrite($file, $this->xml->asXML());
     }
 }
